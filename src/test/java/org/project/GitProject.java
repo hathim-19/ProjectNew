745cb73485b378160709c37b9b149e0159b5342f
hathim-19 <hathim008@gmail.com>
2024-01-13 19:34:04 +0530

    git code

---
+++ src/test/java/org/project/GitProject.java
@@ -0,0 +1,30 @@
+package org.project;
+
+
+
+public class GitProject {
+	
+
+		
+		
+		private void testA1() {
+		  System.out.println("TestA1");
+
+		}
+		
+		
+		private void testA2() {
+		  System.out.println("TestA2");
+
+		}
+		
+		private void testA3() {
+		 	
+		  System.out.println("TestA3");
+
+		}
+
+	}
+
+
+
